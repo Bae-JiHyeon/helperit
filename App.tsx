1861520697626546f57bcf2e33d0a5d13dd971ce
Taeho Kim <livelikeq23010@gmail.com>
2023-06-08 15:21:02 +0900

    test commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import NativeBase1 from './NativeBase1';
+import Login from './Login';
+
+const App = () => {
+  return <Login />;
+};
+
+export default App;
---
+++ Login.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {Container, NativeBaseProvider, HStack, Image} from 'native-base';
+
+//상단에 위치할 AppBar > 로고와 돋보기 버튼, 지도 버튼 들어갈 예정//
+function AppBar() {
+  return (
+    <>
+      <StatusBar backgroundColor="#34BEBA" hidden={true} />
+      <HStack
+        bg="#34BEBA"
+        px="1"
+        py="3"
+        justifyContent="space-between"
+        alignItems="center"
+        w={'100%'}
+        maxW={'100%'}
+        h={60}>
+        <HStack alignItems="center" marginTop={3}>
+          <Image
+            source={require('./Assets/helperItLogo.png')}
+            alt="helperItLogo"
+            size="md"
+            resizeMode="contain"
+            marginLeft={7}
+          />
+        </HStack>
+        <HStack />
+      </HStack>
+    </>
+  );
+}
+
+const NativeBase1 = () => {
+  return (
+    <NativeBaseProvider>
+      <Container>
+        <AppBar />
+      </Container>
+    </NativeBaseProvider>
+  );
+};
+
+export default NativeBase1;
