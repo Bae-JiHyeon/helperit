86511f2947ee1a5213185d2bf99c459a15063e69
Taeho Kim <livelikeq23010@gmail.com>
2023-07-05 14:28:27 +0900

    일거리 요청 페이지

--- UserMode_Screens/RequestPage.tsx
+++ UserMode_Screens/RequestPage.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+//일거리 요청 정보 작성하는 페이지
+//수행지, 도착지 모양 설정하기
+//일거리 요청하기 버튼으로 다음 페이지랑 이어지도록 만들기
+
+import React, {useState} from 'react';
 import {
   Box,
+  Button,
   Heading,
   Input,
   NativeBaseProvider,
@@ -9,7 +14,8 @@ import {
   VStack,
 } from 'native-base';
 import {SelectList} from 'react-native-dropdown-select-list';
-import {TextInput} from 'react-native';
+import {ScrollView, TextInput} from 'react-native';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 
 const RequestPage = () => {
   const [selected, setSelected] = React.useState('');
@@ -30,49 +36,133 @@ const RequestPage = () => {
 
   return (
     <NativeBaseProvider>
-      <Box>
-        <VStack p={6} space={5}>
-          <Heading>카테고리</Heading>
-          <SelectList
-            placeholder={'옵션을 선택해주세요.'}
-            search={false}
-            setSelected={setSelected}
-            data={data_categories}
-          />
-          <Heading>수행일시</Heading>
-          <SelectList
-            placeholder={'옵션을 선택해주세요.'}
-            search={false}
-            setSelected={setSelected}
-            data={data_timing}
-          />
-          <Heading>물품 구매비</Heading>
-          <SelectList
-            placeholder={'옵션을 선택해주세요.'}
-            search={false}
-            setSelected={setSelected}
-            data={data_isNeedPurchase}
-          />
-          {selected === 'true' && (
-            <>
-              <Input placeholder={'예상 구매 가격을 입력하세요'} />
-            </>
-          )}
-          <VStack>
-            <Heading>일거리 요청 내용</Heading>
-            <Text fontSize={'xs'} paddingLeft={3}>
-              *내용이 상세할수록 일거리 수행이 신속하고 정확하게 수행됩니다.
+      <ScrollView>
+        <Box>
+          <VStack p={6} space={5}>
+            <Heading>카테고리</Heading>
+            <SelectList
+              placeholder={'옵션을 선택해주세요.'}
+              search={false}
+              setSelected={setSelected}
+              data={data_categories}
+            />
+            <Heading>수행일시</Heading>
+            <SelectList
+              placeholder={'옵션을 선택해주세요.'}
+              search={false}
+              setSelected={setSelected}
+              data={data_timing}
+            />
+            <Heading>물품 구매비</Heading>
+            <SelectList
+              placeholder={'옵션을 선택해주세요.'}
+              search={false}
+              setSelected={setSelected}
+              data={data_isNeedPurchase}
+            />
+            {selected === 'true' && (
+              <>
+                <Input placeholder={'예상 구매 가격을 입력하세요'} />
+              </>
+            )}
+            <VStack>
+              <Heading>일거리 요청 내용</Heading>
+              <Text fontSize={'xs'} paddingLeft={3}>
+                *내용이 상세할수록 일거리 수행이 신속하고 정확하게 수행됩니다.
+              </Text>
+            </VStack>
+            <TextArea
+              placeholder={'원하시는 요청 내용을 입력해주세요'}
+              autoCompleteType="off"></TextArea>
+            <Heading>수행지</Heading>
+            <GooglePlacesAutocomplete
+              styles={{
+                container: {
+                  flex: 1,
+                },
+                textInputContainer: {
+                  flexDirection: 'row',
+                },
+                textInput: {
+                  backgroundColor: '#FFFFFF',
+                  height: 44,
+                  borderRadius: 5,
+                  paddingVertical: 5,
+                  paddingHorizontal: 10,
+                  fontSize: 15,
+                  flex: 1,
+                },
+                poweredContainer: {
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                  borderBottomRightRadius: 5,
+                  borderBottomLeftRadius: 5,
+                  borderColor: '#c8c7cc',
+                  borderTopWidth: 0.5,
+                },
+                powered: {},
+                listView: {},
+                row: {
+                  backgroundColor: '#FFFFFF',
+                  padding: 13,
+                  height: 44,
+                  flexDirection: 'row',
+                },
+                separator: {
+                  height: 0.5,
+                  backgroundColor: '#c8c7cc',
+                },
+                description: {},
+                loader: {
+                  flexDirection: 'row',
+                  justifyContent: 'flex-end',
+                  height: 20,
+                },
+              }}
+              placeholder="수행지를 입력하세요."
+              onPress={(data, details = null) => {
+                // 'details' is provided when fetchDetails = true
+                console.log(data, details);
+              }}
+              query={{
+                key: 'AIzaSyCBTUMAu0Vl6TthTX6Hv7CT4Fdc-FOXDQ4',
+                language: 'kor',
+              }}
+            />
+            <Heading>도착지</Heading>
+            <GooglePlacesAutocomplete
+              placeholder="도착지를 입력하세요."
+              onPress={(data, details = null) => {
+                // 'details' is provided when fetchDetails = true
+                console.log(data, details);
+              }}
+              query={{
+                key: 'AIzaSyCBTUMAu0Vl6TthTX6Hv7CT4Fdc-FOXDQ4',
+                language: 'kor',
+              }}
+            />
+            <Heading>기타 정보</Heading>
+            <Input
+              h={10}
+              placeholder={'현관 비번 및 추가 정보를 입력해주세요.'}
+            />
+            <Heading>가격</Heading>
+            <Input
+              h={10}
+              placeholder={'최소 일거리 주문 비용 10,000원 부터.'}
+            />
+
+            <Text fontSize={'xs'} paddingLeft={5}>
+              매칭 수수료 1,400원{'\n'}총 일거리 비용 11,400원
             </Text>
+            <Button>
+              <Text bold color={'white'} fontSize={'xl'}>
+                일거리 요청하기
+              </Text>
+            </Button>
           </VStack>
-          <TextArea
-            placeholder={'원하시는 요청 내용을 입력해주세요'}
-            autoCompleteType="off"></TextArea>
-          <Heading>수행지</Heading>
-          <Heading>도착지</Heading>
-          <Heading>기타 정보</Heading>
-          <Heading>가격</Heading>
-        </VStack>
-      </Box>
+        </Box>
+      </ScrollView>
     </NativeBaseProvider>
   );
 };
