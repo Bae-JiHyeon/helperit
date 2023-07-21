679006e1dab570ce53e82a67aec506e31a2cf4bf
Kwak DongHyun <ehdgus2448@gmail.com>
2023-07-21 16:36:06 +0900

    코드 수정

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@ import PrimaryInfoSpec from "./src/screens/JoinHelper/PrimaryInfoSpec";
 import PrimaryInfoBank from "./src/screens/JoinHelper/PrimaryInfoBank";
 import TermsModal from "./src/screens/JoinHelper/TermsModal";
 import JoinHelperStack from "./src/screens/JoinHelper/JoinHelperStack";
+import Map from "./src/Components/Map";
 
 const App = () => {
-  return <JoinHelperStack/>
+  return <Map/>
 
 };
 
